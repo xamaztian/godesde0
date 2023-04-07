7b816ddaad932dc0487a88e607f47e51a53835d8
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2023-04-07 16:41:06 -0400

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xamaztian/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xamaztian/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
