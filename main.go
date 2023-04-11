0d1b9ce3a1792d8054ff01d13431aa9992ff72f0
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2023-04-11 12:37:33 -0400

    Siguiente commit

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/xamaztian/godesde0/ejercicios"
+	"github.com/xamaztian/godesde0/teclado"
 )
 
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(321458)
 	fmt.Println(estado)
-	fmt.Println(texto)*/
+	fmt.Println(texto)
 
 	numero, texto := ejercicios.CalculoEjercicioUno("a98")
 
 	fmt.Println("El número es ", numero)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+
+	teclado.IngresoNumeros()
 }
