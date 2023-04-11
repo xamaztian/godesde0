d0fe1174f3158d5d94f2a0afa86a95328cf91ab4
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2023-04-10 20:20:11 -0400

    SegundoCommit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/xamaztian/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(321458)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
